893bdd42dad43da674213dc2fc6b680d169145b7
azkamiftah12 <azkamiftah29@gmail.com>
2023-11-23 00:20:47 +0700

    test git changes to hosting

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
+            ini test ci/cd
             <h1>Hello, {{ $user->nama }}</h1>
             <p>NIK: {{ $user->nik }}</p>
             <p>No Telp: {{ $user->noTelp }}</p>
