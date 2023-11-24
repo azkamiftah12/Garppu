58f4cefd030a7c5d4509b6661c82bad9dd395640
azkamiftah12 <azkamiftah29@gmail.com>
2023-11-25 00:02:03 +0700

    fix navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,86 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark border-bottom border-dark navbar-bg fixed-top">
+        <div class="container">
+            <a class="navbar-brand mr-auto" href="/dashboard"><img src="{{ asset('images/garppu-logo.png') }}"
+                    alt="Garppu Logo" width="120" height="80"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end navbar-bg" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <a class="navbar-brand mr-auto" href="/dashboard"><img src="{{ asset('images/garppu-logo.png') }}"
+                            alt="Garppu Logo" width="120" height="80"></a>
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav">
+                        <li class="nav-item {{ Request::is('dashboard') ? 'active' : '' }}">
+                            <a class="nav-link mx-3" href="{{ url('/dashboard') }}">Home</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('subrelawan') ? 'active' : '' }}">
+                            <a class="nav-link mx-3" href="{{ url('/subrelawan') }}">AnggotaKu</a>
+                        </li>
+                        <li class="nav-item {{ Request::is('quickcount') ? 'active' : '' }}">
+                            <a class="nav-link mx-3" href="{{ url('/quickcount') }}">Quick Count</a>
+                        </li>
+                        <li class="nav-item {{ request()->is('help') ? 'active' : '' }}">
+                            <a class="nav-link mx-3" href="{{ url('/help') }}">Help</a>
+                        </li>
+                        <div class="btn-logout-mobile">
 
-    <nav class="navbar navbar-expand-lg border-bottom border-dark navbar-bg fixed-top">
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-red my-5">Logout</button>
+                            </form>
+                        </div>
+                    </ul>
+                </div>
+                {{-- <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown"
+                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div> --}}
+            </div>
+            <div class="ml-auto btn-logout-large">
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-red">Logout</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    {{-- <nav class="navbar navbar-expand-lg border-bottom border-dark navbar-bg fixed-top">
         <div class="container">
             <a class="navbar-brand mr-auto" href="#"><img src="{{ asset('images/garppu-logo.png') }}"
-                    alt="Garppu Logo" width="120" height="80"></a>
+                alt="Garppu Logo" width="120" height="80"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -45,7 +120,7 @@
                 </form>
             </div>
         </div>
-    </nav>
+    </nav> --}}
 
 
     <div class="container container-default">
