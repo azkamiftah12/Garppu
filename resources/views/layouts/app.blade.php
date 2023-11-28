fbd08436cba8b1df80e9270e3b88fc259cd09d92
azkamiftah12 <90744201+azkamiftah12@users.noreply.github.com>
2023-11-28 23:36:25 +0700

    adding head meta in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
 
 <head>
     <title>Garppu</title>
+    <meta name="description" content="Garppu merupakan singkatan dari Gerakan Pemantau Pemilu">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{ asset('images/garppu-logo.png') }}" type="image/icon type">
+    <link rel="icon" href="{{ asset('images/garppu-logo.png') }}" type="image/x-icon>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
