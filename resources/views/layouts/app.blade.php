aa391214a09e6038d1f9858ae807bcdc69bed908
azkamiftah12 <azkamiftah29@gmail.com>
2023-12-03 22:13:23 +0700

    minor margin update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,11 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <style>
+        .dataTables_length {
+            margin-bottom: 20px;
+        }
+    </style>
 </head>
 
 <body>
