c43e6e14dc4f4ac34bb2bab5b32e0d0cbe960ff4
Fadh29 <fadhlurahmanraihan29@gmail.com>
2023-12-17 20:58:25 +0700

    remove fk batch from tbl partais

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SuperAdminController;
 use App\Http\Controllers\DapilController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SubRelawanController;
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\BatchController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,7 +19,23 @@ use App\Http\Controllers\DapilController;
 |
 */
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::resource('subrelawan', SubRelawanController::class);
+Route::resource('userprofiles', UserProfileController::class);
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
+
+// Batch routes
+Route::get('/batches', [BatchController::class, 'index'])->name('superadmin.batches.index');
+Route::get('/batches/create', [BatchController::class, 'create'])->name('superadmin.batches.create');
+Route::post('/batches', [BatchController::class, 'store'])->name('superadmin.batches.store');
+Route::get('/batches/{batch}/edit', [BatchController::class, 'edit'])->name('superadmin.batches.edit');
+Route::put('/batches/{batch}', [BatchController::class, 'update'])->name('superadmin.batches.update');
+Route::delete('/batches/{batch}', [BatchController::class, 'destroy'])->name('superadmin.batches.destroy');
 
 Route::get('/superadmin/dapil', [DapilController::class, 'index'])->name('superadmin.dapil.index');
 Route::get('/superadmin/dapil/create', [DapilController::class, 'create'])->name('superadmin.dapil.create');
