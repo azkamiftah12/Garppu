66a38e24b83d31295e8e17c166a6ef6402bc739f
azkamiftah12 <azkamiftah29@gmail.com>
2024-01-08 14:42:33 +0700

    update admin layout and progress bar in admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,8 +13,29 @@ class AdminController extends Controller
 {
     public function dashboard()
     {
-        $user = Auth::user(); // Retrieve the authenticated user
-        return view('admin.dashboard', compact('user'));
+        $user = Auth::user();
+
+        $relawans = User::withCount('subRelawans')
+        ->where('userRole', 'relawan')
+        ->where('id_dapil', $user->id_dapil)
+        ->get();
+
+        $subRelawans = SubRelawan::leftJoin('userprofile', 'sub_relawans.nik', '=', 'userprofile.nik')
+        ->where('userprofile.id_dapil', $user->id_dapil)
+        ->get();
+
+        $totalRelawans = $relawans->count();
+        $totalSubRelawans = $subRelawans->count();
+
+        $targetRelawans = 1000;
+        $targetSubRelawans = $totalRelawans * 50;
+        $totaltargetSubRelawans = 1000 * 50;
+
+        $progressRelawans = $totalRelawans/$targetRelawans*100;
+        $progressSubRelawans = $totalSubRelawans/$targetSubRelawans*100;
+        $progresstotalSubRelawans = $totalSubRelawans/$totaltargetSubRelawans*100;
+
+        return view('admin.dashboard', compact('user', 'relawans', 'totalRelawans', 'totalSubRelawans', 'targetSubRelawans', 'targetRelawans', 'progressRelawans', 'progressSubRelawans', 'totaltargetSubRelawans', 'progresstotalSubRelawans'));
     }
     public function getUsersRelawan()
 {
--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <h1 class="mb-5">Selamat Datang di Admin Page, {{ $user->nama }}</h1>
-            <h5>NIK: {{ $user->nik }}</h5>
-            <h5>No Telp: {{ $user->noTelp }}</h5>
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="mb-5">Selamat Datang di Admin Page, {{ $user->nama }}</h1>
+                    <h5>NIK: {{ $user->nik }}</h5>
+                    <h5>No Telp: {{ $user->noTelp }}</h5>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
