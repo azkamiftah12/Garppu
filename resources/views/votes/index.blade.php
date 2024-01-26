77f8e87056adad7ca377dd676cd9727c6db8f215
azkamiftah12 <azkamiftah29@gmail.com>
2024-01-26 22:09:59 +0700

    fix if batch dont have candidate then show nothing

--- resources/views/votes/index.blade.php
+++ resources/views/votes/index.blade.php
@@ -8,34 +8,37 @@
                     <div class="card-body">
                         <h1 class="mb-5 text-center">Halaman Voting</h1>
                         @foreach ($batches as $batch)
-                            <div class="border-0 shadow rounded col-md-12 p-4 mb-5"
-                                style="background-color: var(--color-white-brown)">
-                                <h1 class="text-center">Hasil QuickCount {{ $batch->vote_type }}</h1>
+                            @if ($batch->candidates->isEmpty())
+                            @else
+                                <div class="border-0 shadow rounded col-md-12 p-4 mb-5"
+                                    style="background-color: var(--color-white-brown)">
+                                    <h1 class="text-center">Hasil QuickCount {{ $batch->vote_type }}</h1>
 
-                                {{-- Tampilkan total hasil jumlah_vote per kandidat --}}
-                                {{-- @foreach ($candidatesWithVotesDPRD as $candidate)
-                                    <div class="mb-3">
-                                        <h4>Nama Paslon: {{ $candidate->name ?? '-' }}</h4>
-                                        <p>Partai: {{ $candidate->nama_partai ?? '-' }}</p>
-                                        <p>Total Votes: {{ $candidate->votes->sum('jumlah_vote') ?? '-' }}</p>
-                                    </div>
-                                @endforeach --}}
-
-
-                                @foreach ($batch->candidates as $candidate)
-                                    <div class="mb-3">
-                                        <h4>Nama Paslon: {{ $candidate->name ?? '-' }}</h4>
-                                        <p>Partai: {{ $candidate->nama_partai ?? '-' }}</p>
-                                        <p>Total Votes: {{ $candidate->votes->sum('jumlah_vote') ?? '-' }}</p>
-                                    </div>
-                                @endforeach
-
-                                <div class="d-flex justify-content-center my-3">
-                                    <a class="btn btn-soft-blue flex-fill" style="max-width: 350px"
-                                        href="{{ route('votes.showVote', $batch->id) }}">Input Vote
-                                        {{ $batch->vote_type }}</a>
+                                    {{-- Tampilkan total hasil jumlah_vote per kandidat --}}
+                                    {{-- @foreach ($candidatesWithVotesDPRD as $candidate)
+                                <div class="mb-3">
+                                    <h4>Nama Paslon: {{ $candidate->name ?? '-' }}</h4>
+                                    <p>Partai: {{ $candidate->nama_partai ?? '-' }}</p>
+                                    <p>Total Votes: {{ $candidate->votes->sum('jumlah_vote') ?? '-' }}</p>
                                 </div>
-                            </div>
+                            @endforeach --}}
+
+
+                                    @foreach ($batch->candidates as $candidate)
+                                        <div class="mb-3">
+                                            <h4>Nama Paslon: {{ $candidate->name ?? '-' }}</h4>
+                                            <p>Partai: {{ $candidate->nama_partai ?? '-' }}</p>
+                                            <p>Total Votes: {{ $candidate->votes->sum('jumlah_vote') ?? '-' }}</p>
+                                        </div>
+                                    @endforeach
+
+                                    <div class="d-flex justify-content-center my-3">
+                                        <a class="btn btn-soft-blue flex-fill" style="max-width: 350px"
+                                            href="{{ route('votes.showVote', $batch->id) }}">Input Vote
+                                            {{ $batch->vote_type }}</a>
+                                    </div>
+                                </div>
+                            @endif
                         @endforeach
                     </div>
                 </div>
